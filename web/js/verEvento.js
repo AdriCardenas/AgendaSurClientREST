ed4014f0e322b691d180a60f2abc88dc8c60559b
Adrian <cardenitas96@gmail.com>
2017-12-09 12:20:46 +0100

    Añadido google maps api

--- web/js/verEvento.js
+++ web/js/verEvento.js
@@ -3,8 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var latitud, longitud, nombreEvento;
 
-$(document).ready(function(){
+
+$(document).ready(function () {
     var retrievedObject = localStorage.getItem('evento');
     console.log('retrievedObject: ', JSON.parse(retrievedObject));
-})
+
+    var jsonEvento = JSON.parse(retrievedObject);
+
+    nombreEvento = jsonEvento.nombre;
+
+    document.getElementById('nombreEvento').innerHTML = nombreEvento;
+    document.getElementById('descripcionEvento').innerHTML = jsonEvento.descripcion;
+    document.getElementById('fechaInicio').innerHTML = jsonEvento.fechainicio;
+    document.getElementById('fechaFin').innerHTML = jsonEvento.fechafin;
+    document.getElementById('direccionEvento').innerHTML = jsonEvento.direccion;
+
+    latitud = jsonEvento.latitud;
+    longitud = jsonEvento.longitud;
+
+});
+
+
