5bccd387453e87daf775808f6abb3f85f2d8c3b2
Garlo13git config --global user.email adri_presim301@hotmail.comgit config --global user.name Garlo13 <adri_presim301@hotmail.com>
2017-12-10 18:36:33 +0100

    Añadido carousel de imágenes en verEvento.html

--- web/js/verEvento.js
+++ web/js/verEvento.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var latitud, longitud, nombreEvento;
+var latitud, longitud, nombreEvento, tags;
 
 
 $(document).ready(function () {
@@ -20,11 +20,26 @@ $(document).ready(function () {
     document.getElementById('fechaFin').innerHTML = jsonEvento.fechafin;
     document.getElementById('direccionEvento').innerHTML = jsonEvento.direccion;
 
+
+    tags = jsonEvento.tags;
+    console.log(tags);
+
     latitud = parseFloat(jsonEvento.latitud);
     longitud = parseFloat(jsonEvento.longitud);
 
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = handleResponse;
+    //var lugar = document.getElementById("lugarid").value;
+    var API_KEY = "ce467785e4e6b7dc282e86e0f2268c26";
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
+            "&format=json&api_key=" + API_KEY + "&tags=" + tags;
+    console.log(url);
+    xhttp.open("GET", url, true);
+    xhttp.send(null)
+
     initMap();
-    
+
     function initMap() {
         map = new google.maps.Map(document.getElementById('map-canvas'), {
             center: new google.maps.LatLng(latitud, longitud),
@@ -47,6 +62,57 @@ $(document).ready(function () {
             infowindow.open(map, marker);
         });
     }
+
+
+    ;
+
+    function handleResponse() {
+
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            var response = xhttp.responseText;
+            var substring = "jsonFlickrApi";
+            var idx = response.indexOf(substring);
+            var response = response.substring(idx + substring.length + 1, response.length - 1);
+            var texto = JSON.parse(response);
+            console.log(texto);
+            var fotos = texto.photos.photo;
+            urlimg = 'http://farmFARM.staticflickr.com/SERVER/ID_SECRET_SIZE.jpg';
+            var elem;
+            var urlimg;
+            var FARM;
+            var SERVER;
+            var ID;
+            var SECRET;
+            var SIZE = 'z';
+            var i = 0;
+            //for (var i = 0; i < fotos.length; i++) {
+            while (i < 3) {
+                elem = document.createElement('img');
+                FARM = fotos[i].farm;
+                SERVER = fotos[i].server;
+                ID = fotos[i].id;
+                SECRET = fotos[i].secret;
+                urlimg = 'http://farm' + FARM + '.staticflickr.com/' + SERVER + '/' + ID + '_' + SECRET + '_' + SIZE + '.jpg';
+                elem.src = urlimg;
+                document.getElementById("foto" + i).appendChild(elem);
+                //document.body.appendChild(elem);
+                i++;
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 });
 
