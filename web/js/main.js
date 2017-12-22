b0b4d913abfd1801429ba7e72644d5d3726c2b1f
davidluque <davidluque@192.168.1.9>
2017-12-22 20:00:34 +0100

    Logout

--- web/js/main.js
+++ web/js/main.js
@@ -308,6 +308,46 @@ function handleSignOutClick() {
     window.location = "index.html";
 }
 
+// GOOGLE
 
 
+function handleClientLoad() {
+    // Loads the client library and the auth2 library together for efficiency.
+    // Loading the auth2 library is optional here since `gapi.client.init` function will load
+    // it if not already loaded. Loading it upfront can save one network request.
+    gapi.load('client:auth2', initClient);
+}
+
+function handleConnection() {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+}
+
+function initClient() {
+    // Initialize the client with API key and People API, and initialize OAuth with an
+    // OAuth 2.0 client ID and scopes (space delimited string) to request access.
+    var initialized = gapi.client.init({
+        apiKey: 'AIzaSyD-YkjNcqoEH4GND279kbs8YkW0AF5J3pg',
+        discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+        clientId: '1095663528592-3uacq2gga1bien0l9jqfh22i1o6qaf7f.apps.googleusercontent.com',
+        scope: 'profile'
+    });
+    initialized.then(handleConnection);
+}
+
+function updateSigninStatus(isSignedIn) {
+    // When signin status changes, this function is called.
+    // If the signin status is changed to signedIn, we make an API call.
+    if (isSignedIn) {
+        //alert('Conectado correctamente');
+        gapi.auth2.getAuthInstance().disconnect();
+        localStorage.removeItem('usuarioSesion');
+        window.location = "index.html";
+        //addUser();
+
+    }
+}
 
